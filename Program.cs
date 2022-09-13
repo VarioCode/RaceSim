655b2ad6b7f64389fe27af31c1b2f636356b8d4d
JMC-Cyber <jmccrans@gmail.com>
2022-09-13 23:20:59 +0200

    Project starter

---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Welcome to this race sim thing that we have to make");
