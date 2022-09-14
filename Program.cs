0d06f0f61872e6605d23fff602de71d6f20d607b
JMC-Cyber <jmccrans@gmail.com>
2022-09-14 11:16:49 +0200

    Created Project
    + Controller Class Library
    + Model Class Library
    + ControllerTests (Unit Tests)

--- Program.cs
+++ Program.cs
@@ -1,8 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, Welcome to this race sim thing that we have to make");
-
-if (true)
-{
-    Console.WriteLine();
-}
